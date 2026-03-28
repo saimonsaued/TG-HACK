2f31b3a9bc4f2744e432b374ae2dc2e815952e72
saimonsaued <mdjahedkargil@gmail.com>
2026-03-28 23:14:05 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Server running 😎"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
